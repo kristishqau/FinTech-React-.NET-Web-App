9dd337a2b43b196b6ea9a7e7d543ff4c9880c666
Kristi Shqau <shqaukristi4@gmail.com>
2025-03-25 20:44:02 +0100

    add POST method

--- backend/Dtos/Stock/StockDto.cs
+++ backend/Dtos/Stock/StockDto.cs
@@ -8,11 +8,11 @@ namespace api.Dtos.Stock
     public class StockDto
     {
         public int Id { get; set; }
-        public string Symbol { get; set; } = string.Empty;
-        public string CompanyName { get; set; } = string.Empty;
+        public string? Symbol { get; set; }
+        public string? CompanyName { get; set; }
         public decimal Purchase { get; set; }
         public decimal LastDividend { get; set; }
-        public string Industry { get; set; } = string.Empty;
+        public string? Industry { get; set; }
         public long MarketCap { get; set; }
     }
 }
