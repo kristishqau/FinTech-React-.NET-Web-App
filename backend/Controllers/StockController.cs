561fe84a28359297e34b7ccb4e62c00b5888712a
Kristi Shqau <shqaukristi4@gmail.com>
2025-03-31 12:52:32 +0200

    add update and delete

--- backend/Controllers/StockController.cs
+++ backend/Controllers/StockController.cs
@@ -34,7 +34,7 @@ namespace backend.Controllers
         public IActionResult GetById([FromRoute] int id)
         {
             var stock = _context.Stocks.Find(id);
-            if(stock == null)
+            if (stock == null)
             {
                 return NotFound();
             }
@@ -48,6 +48,41 @@ namespace backend.Controllers
             _context.Stocks.Add(stockModel);
             _context.SaveChanges();
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDividend = updateDto.LastDividend;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+            _context.SaveChanges();
+
+            return Ok(stockModel.ToStockDto());
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            if(stockModel == null)
+            {
+                return NotFound();
+            }
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+            return NoContent(); 
         }
     }
 }
